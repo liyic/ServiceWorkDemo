2083a9d02f23780f786ea0129db7197c7ac2586e
liyic <prolovecui@126.com>
2016-02-25 11:34:07 +0800

    Added files via upload

---
+++ worker.js
@@ -0,0 +1,7 @@
+﻿addEventListener("fetch", function(evt) {
+  evt.respondWith(new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(new Response(“Hi, Barret Lee”));
+    }, 1000);
+  }));
+});
